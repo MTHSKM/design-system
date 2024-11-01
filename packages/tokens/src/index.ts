02e5171bcbb7c5015408382ba639ac4b7870b742
MTHSKM <matheuskim356@gmail.com>
2024-11-01 13:25:34 -0300

    Configurando tokens

--- packages/tokens/src/index.ts
+++ packages/tokens/src/index.ts
@@ -1,5 +1,9 @@
-export * from "./colors"
+export * from './colors'
 
-// import { colors } from "@ignite-ui/tokens/colors"
-// import { colors } from "@ignite-ui/tokens"
+export * from './space'
+export * from './radii'
 
+export * from './fonts'
+export * from './font-sizes'
+export * from './font-weights'
+export * from './line-heights'
