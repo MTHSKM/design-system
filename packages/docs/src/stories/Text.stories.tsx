572e85ea8f013974843dd87f70b1810aace97c38
MTHSKM <matheuskim356@gmail.com>
2024-11-06 14:16:46 -0300

    componente:Text

---
+++ packages/docs/src/stories/Text.stories.tsx
@@ -0,0 +1,18 @@
+import { Text, TextProps } from '@ignite-ui/react'
+import type { Meta, StoryObj } from '@storybook/react'
+
+export default {
+  title: 'Typography/Text',
+  component: Text,
+  args: {
+    children: 'Example Text',
+  },
+} as Meta<TextProps>
+
+export const Primary: StoryObj<TextProps> = {}
+export const CustomTag: StoryObj<TextProps> = {
+  args: {
+    children: 'Strong Text',
+    as: 'strong',
+  },
+}
